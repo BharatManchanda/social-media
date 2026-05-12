3adf92fb71c13082733413b816d5d98cb2058a8f
Bharat Manchanda <bharatmanchanda13@gmail.com>
2026-05-12 09:10:11 +0530

    update next js

--- app/api/get-me/route.ts
+++ app/api/get-me/route.ts
@@ -1,27 +1,26 @@
 import { prisma } from "../../../lib/prisma";
-import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
     try {
-        // const req.cookies.get("token")?.value;
+        const userId = req.headers.get("x-user-id");
         const user = await prisma.user.findFirst({
             where: {
-                id: req.user.id
+                id: userId as any
             }
         });
         return Response.json(
-			{
-				message: "Get me successfully",
+            {
+                message: "Get me successfully",
                 user: user
-			}
-		);
+            }
+        );
     } catch (error) {
         return Response.json(
-			{
-				message: "Something went wrong",
-				error: String(error)
-			},
-			{ status: 422 }
-		);
+            {
+                message: "Something went wrong",
+                error: String(error)
+            },
+            { status: 422 }
+        );
     }
 }
